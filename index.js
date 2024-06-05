3086a319022c61d921c8790ff34754b4371bf221
italo macedo <macedoitalo21@gmail.com>
2024-06-05 20:42:12 -0300

    Paginação de resultados

--- index.js
+++ index.js
@@ -5,16 +5,19 @@ const app = express()
 
 app.use(express.json())
 
-app.get('/:id', async (req, res) => {
-    const { id } = req.params
+app.get('/', async (req, res) => {
+    const {pagina,porPagina} = req.query
+    //const { id } = req.params
     try {
         // const query = `select * from empresas where nome = $1 or nome = $2`
         // const params = ['Google', 'Facebook']
-        const query = 'update empresas set site = $1 where id = $2'
-        const params = ['wwww.cakewalk.com', 1]
-        const resultado = await pool.query(query, params)
+        // const query = 'update empresas set site = $1 where id = $2'
+        // const params = ['wwww.cakewalk.com', 1]
+        const query = 'select * from pessoas order by id asc limit $1 offset $2'
+        const offset = (pagina - 1) * porPagina;
+        const resultado = await pool.query(query, [porPagina,offset])
         //await pool.end()
-        return res.json(resultado)
+        return res.json(resultado.rows)
     } catch (error) {
         console.log(error.message)
     }
